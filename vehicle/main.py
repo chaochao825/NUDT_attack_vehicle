bc2d984ca1a24f77db8ecb6cd9d770afdd0d5143
superchao <xuych56@mail2.sysu.edu.cn>
2025-10-25 12:23:54 +0000

    fix type switch

--- vehicle/main.py
+++ vehicle/main.py
@@ -33,16 +33,16 @@ def parse_args():
     parser.add_argument('--input_path', type=str, default='./input', help='input path')
     parser.add_argument('--output_path', type=str, default='./output', help='output path')
     
-    parser.add_argument('--process', type=str, default='train', help='[adv, attack, defend, train]')
+    parser.add_argument('--process', type=str, default='attack', help='[adv, attack, defend, train]')
     parser.add_argument('--model', type=str, default='yolov8', help='model name')
-    parser.add_argument('--data', type=str, default='imagenet10', help='data name')
-    # parser.add_argument('--data', type=str, default='coco8', help='data name')
+    # parser.add_argument('--data', type=str, default='imagenet10', help='data name')
+    parser.add_argument('--data', type=str, default='coco8', help='data name')
     
     parser.add_argument('--attack_method', type=str, default='cw', help='attack method [cw, deepfool, bim, fgsm, pdg]')
     parser.add_argument('--defend_method', type=str, default=None, help='defend method')
     
-    parser.add_argument('--task', type=str, default='classify', help='task name')
-    # parser.add_argument('--task', type=str, default='detect', help='task name')
+    # parser.add_argument('--task', type=str, default='classify', help='task name')
+    parser.add_argument('--task', type=str, default='detect', help='task name')
     parser.add_argument('--cfg_path', type=str, default='./cfgs', help='cfg path')
     
     parser.add_argument('--epochs', type=int, default=100, help='epochs')
@@ -60,19 +60,20 @@ def parse_args():
     args_dict = vars(args)
     args_dict_environ = {}
     for key, value in args_dict.items():
-        args_dict_environ[key] = type_switch(os.getenv(key.upper(), value))
+        value_type = type(value)
+        args_dict_environ[key] = type_switch(os.getenv(key.upper(), value), value_type)
     args_easydict = EasyDict(args_dict_environ)
     return args_easydict
 
 
-def type_switch(environ_value):
-    if isinstance(environ_value, int):
+def type_switch(environ_value, value_type):
+    if isinstance(value_type, int):
         return int(environ_value)
-    elif isinstance(environ_value, float):
+    elif isinstance(value_type, float):
         return float(environ_value)
-    elif isinstance(environ_value, bool):
+    elif isinstance(value_type, bool):
         return bool(environ_value)
-    elif isinstance(environ_value, str):
+    elif isinstance(value_type, str):
         return environ_value
     
 def yolo_cfg(args):
